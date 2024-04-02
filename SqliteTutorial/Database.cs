e306542269ff54a30524ce91bb9379b378c684c8
MasnaKaczucha <kwiecse21@sps-prosek.cz>
2024-04-02 09:19:13 +0200

    Přidejte soubory projektu.

---
+++ SqliteTutorial/Database.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using System.IO;
+
+namespace SqliteTutorial
+{
+    public class Database
+    {
+
+        private SQLiteAsyncConnection _connection;
+
+        public Database(string path) 
+        { 
+            // Path.Combine("", path) prej lepsi nez (path)
+            _connection = new SQLiteAsyncConnection(Path.Combine("", path));
+            // async = proces se zpracovava vedle, wait pocka az proces skonci mimo hlavni vlakno
+            _connection.CreateTableAsync<Avenger>().Wait();
+        }
+
+        public Task<List<Avenger>> GetItemsAsync()
+        {
+            return _connection.Table<Avenger>().ToListAsync();
+        }
+
+        public Task<int> SaveItemAsync(Avenger item)
+        {
+            // you know you need to update
+            if(item.Id != 0)
+            {
+                return _connection.UpdateAsync(item);
+
+            }
+            // else insert
+            else
+            {
+                return _connection.InsertAsync(item);
+            }
+            
+        }
+    }
+}
---
+++ SqliteTutorial/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SqliteTutorial
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Database _db;
+        public Database Db 
+        { 
+            get 
+            { 
+                if(_db == null)
+                {
+                    _db = new Database("Avengers.db3");
+                }
+                return _db; 
+            } 
+        }
+
+        public ObservableCollection<Avenger> Items { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Items = new ObservableCollection<Avenger>(Db.GetItemsAsync().Result);
+            ListView1.ItemsSource = Items;
+        }
+
+        private void b1_Click(object sender, RoutedEventArgs e)
+        {
+            Avenger item = new Avenger()
+            {
+                Name = t1.Text,
+                RealName = t2.Text
+            };
+            Db.SaveItemAsync(item);
+            Items.Add(item);
+            t1.Text = "";
+            t2.Text = "";
+
+
+        }
+    }
+}
